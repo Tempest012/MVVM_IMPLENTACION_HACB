0eed2105954099c5f2f0698b6a425f5e6a231f9b
Harol Cisneros <harolcsnrs@gmail.com>
2023-10-20 18:47:32 -0700

    Agregar archivos de proyecto.

---
+++ MVVM_IMPLENTACION_HACB/VistaModelo/VMpatron.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+
+namespace MVVM_IMPLENTACION_HACB.VistaModelo
+{
+    internal class VMpatron:BaseViewModel
+    {
+        #region VARIABLES
+        string _Texto;
+        #endregion
+        #region CONTRUCTOR
+
+        #endregion
+        #region OBJETOS
+        public string Texto
+        {
+            get { return _Texto;}
+            set { SetValue(ref _Texto, value);}
+        }
+        #endregion
+        #region PROCESOS
+        public async Task ProcesoAsyncrono()
+        {
+
+        }
+        public void ProcesoSimple()
+        {
+
+        }
+        #endregion
+    }
+}
