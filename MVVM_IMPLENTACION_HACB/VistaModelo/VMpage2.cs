53d47f0b7e46817eb992d167192b1c24088737b3
Harol Cisneros <harolcsnrs@gmail.com>
2023-11-06 09:03:04 -0700

    Imagenes

--- MVVM_IMPLENTACION_HACB/VistaModelo/VMpage2.cs
+++ MVVM_IMPLENTACION_HACB/VistaModelo/VMpage2.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
+using MVVM_IMPLENTACION_HACB.Modelo;
 
 namespace MVVM_IMPLENTACION_HACB.VistaModelo
 {
@@ -11,11 +12,13 @@ namespace MVVM_IMPLENTACION_HACB.VistaModelo
     {
         #region VARIABLES
         string _Texto;
+        public List<Musuario> listausuario {  get; set; }
         #endregion
         #region CONTRUCTOR
         public VMpage2(INavigation navigation)
         {
             Navigation = navigation;
+            MostraUsuarios();
         }
         #endregion
         #region OBJETOS
@@ -34,10 +37,31 @@ namespace MVVM_IMPLENTACION_HACB.VistaModelo
         {
 
         }
+        public void MostraUsuarios()
+        {
+            listausuario = new List<Musuario>();
+            new Musuario
+            {
+                Nombre = "Furina1",
+                Imagen = "https://i.ibb.co/GMxnP8D/dee6a9929c83caca52904d830909742c.jpg"
+
+            };
+            new Musuario
+            {
+                Nombre = "Furina2",
+                Imagen = "https://i.ibb.co/9pyVhyT/884596c19b8b67c918de4f3fef34521b.jpg"
+            };
+            new Musuario
+            {
+                Nombre = "Furina3",
+                Imagen = "https://i.ibb.co/YZkWVtF/0a3d856d01d52bb7e513122ef43782de.jpg"
+            };
+        }
         #endregion
         #region COMANDOS
         public ICommand Volvercommand => new Command(async () => await Volver());
-        public ICommand ProcesoSimpcommand => new Command(ProcesoSimple);
+        public ICommand MostraUsuarioscommand => new Command(MostraUsuarios);
+        //public ICommand ProcesoSimpcommand => new Command(ProcesoSimple);
         #endregion
     }
 }
