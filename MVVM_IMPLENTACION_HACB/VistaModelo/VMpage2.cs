62d2ec4f597ef13051030f5659aac1a2501169b7
Harol Cisneros <harolcsnrs@gmail.com>
2023-11-09 14:07:53 -0700

    Muestra las imagenes y una ventana de alerta

--- MVVM_IMPLENTACION_HACB/VistaModelo/VMpage2.cs
+++ MVVM_IMPLENTACION_HACB/VistaModelo/VMpage2.cs
@@ -39,29 +39,36 @@ namespace MVVM_IMPLENTACION_HACB.VistaModelo
         }
         public void MostraUsuarios()
         {
-            listausuario = new List<Musuario>();
-            new Musuario
+            listausuario = new List<Musuario>
             {
-                Nombre = "Furina1",
-                Imagen = "https://i.ibb.co/GMxnP8D/dee6a9929c83caca52904d830909742c.jpg"
+                new Musuario
+                {
+                    Nombre = "Furina1",
+                    Imagen = "https://i.ibb.co/GMxnP8D/dee6a9929c83caca52904d830909742c.jpg"
 
+                },
+                new Musuario
+                {
+                    Nombre = "Furina2",
+                    Imagen = "https://i.ibb.co/9pyVhyT/884596c19b8b67c918de4f3fef34521b.jpg"
+                },
+                new Musuario
+                {
+                    Nombre = "Furina3",
+                    Imagen = "https://i.ibb.co/YZkWVtF/0a3d856d01d52bb7e513122ef43782de.jpg"
+                }
             };
-            new Musuario
-            {
-                Nombre = "Furina2",
-                Imagen = "https://i.ibb.co/9pyVhyT/884596c19b8b67c918de4f3fef34521b.jpg"
-            };
-            new Musuario
-            {
-                Nombre = "Furina3",
-                Imagen = "https://i.ibb.co/YZkWVtF/0a3d856d01d52bb7e513122ef43782de.jpg"
-            };
+        }
+        public async Task Alerta()
+        {
+            await DisplayAlert("Titulo", "Mensaje", "ok");
         }
         #endregion
         #region COMANDOS
         public ICommand Volvercommand => new Command(async () => await Volver());
-        public ICommand MostraUsuarioscommand => new Command(MostraUsuarios);
+        //public ICommand MostraUsuarioscommand => new Command(MostraUsuarios);
         //public ICommand ProcesoSimpcommand => new Command(ProcesoSimple);
+        public ICommand Alertacommand => new Command(async () => await Alerta());
         #endregion
     }
 }
